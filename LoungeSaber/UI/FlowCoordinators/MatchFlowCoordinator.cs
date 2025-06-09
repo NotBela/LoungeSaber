a4342c76e36eb0a71494f5306aec27a0b0eaa42e
Bela <27pvarga@gmail.com>
2025-06-09 09:25:44 -0400

    added MatchFlowCoordinator.cs

---
+++ LoungeSaber/UI/FlowCoordinators/MatchFlowCoordinator.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using HarmonyLib;
+using HMUI;
+using LoungeSaber.UI.BSML;
+using Zenject;
+
+namespace LoungeSaber.UI.FlowCoordinators
+{
+    public class MatchFlowCoordinator : FlowCoordinator
+    {
+        [Inject] private readonly GameplaySetupViewController _gameplaySetupViewController = null;
+        
+        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            SetupGameplaySetupViewController();
+        }
+        
+        private void SetupGameplaySetupViewController()
+        {
+            _gameplaySetupViewController.didActivateEvent += OnGameplaySetupViewActivated;
+            
+            _gameplaySetupViewController.Setup(true, true,true, false, PlayerSettingsPanelController.PlayerSettingsPanelLayout.Singleplayer);
+        }
+        
+        protected override void DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling)
+        {
+            ResetGameplaySetupView();
+            
+            _gameplaySetupViewController.didActivateEvent -= OnGameplaySetupViewActivated;
+        }
+        
+        private void ResetGameplaySetupView()
+        {
+            _gameplaySetupViewController._gameplayModifiersPanelController._gameplayModifierToggles.Do(i =>
+                i.gameObject.SetActive(true));
+        }
+        
+        private void OnGameplaySetupViewActivated(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) => _gameplaySetupViewController._gameplayModifiersPanelController._gameplayModifierToggles.Where(i => i.name != "ProMode").Do(i => i.gameObject.SetActive(false));
+    }
+}
--- LoungeSaber/UI/FlowCoordinators/MatchmakingMenuFlowCoordinator.cs
+++ LoungeSaber/UI/FlowCoordinators/MatchmakingMenuFlowCoordinator.cs
@@ -13,43 +13,18 @@ namespace LoungeSaber.UI.FlowCoordinators
     {
         [Inject] private readonly MainFlowCoordinator _mainFlowCoordinator = null;
         
-        [Inject] private readonly GameplaySetupViewController _gameplaySetupViewController = null;
         [Inject] private readonly MatchmakingMenuViewController _matchmakingMenuViewController = null;
 
         protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
         {
-            SetupGameplaySetupViewController();
-            
             showBackButton = true;
-            ProvideInitialViewControllers(_matchmakingMenuViewController, _gameplaySetupViewController);
+            ProvideInitialViewControllers(_matchmakingMenuViewController);
             SetTitle("LoungeSaber");
         }
-
-        private void SetupGameplaySetupViewController()
-        {
-            _gameplaySetupViewController.didActivateEvent += OnGameplaySetupViewActivated;
-            
-            _gameplaySetupViewController.Setup(true, true,true, false, PlayerSettingsPanelController.PlayerSettingsPanelLayout.Singleplayer);
-        }
-
-        protected override void DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling)
-        {
-            _gameplaySetupViewController.didActivateEvent -= OnGameplaySetupViewActivated;
-        }
-
-        private void ResetGameplaySetupView()
-        {
-            _gameplaySetupViewController._gameplayModifiersPanelController._gameplayModifierToggles.Do(i =>
-                i.gameObject.SetActive(true));
-        }
-
-        private void OnGameplaySetupViewActivated(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) => _gameplaySetupViewController._gameplayModifiersPanelController._gameplayModifierToggles.Where(i => i.name != "ProMode").Do(i => i.gameObject.SetActive(false));
 
         protected override void BackButtonWasPressed(ViewController _)
         {
             _mainFlowCoordinator.DismissFlowCoordinator(this);
-            
-            ResetGameplaySetupView();
         }
     }
 }  
