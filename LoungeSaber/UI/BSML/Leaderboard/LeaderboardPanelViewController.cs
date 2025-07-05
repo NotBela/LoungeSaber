ce44e6cf0c30b6bd94c6bf3aac10e299ca31569b
Bela <27pvarga@gmail.com>
2025-07-05 00:11:46 -0400

    more done on leaderboard panel

--- LoungeSaber/UI/BSML/Leaderboard/LeaderboardPanelViewController.cs
+++ LoungeSaber/UI/BSML/Leaderboard/LeaderboardPanelViewController.cs
@@ -1,16 +1,20 @@
 ﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.FloatingScreen;
 using BeatSaberMarkupLanguage.ViewControllers;
 using LoungeSaber.Server;
 using SiraUtil.Logging;
+using UnityEngine;
 using Zenject;
 
 namespace LoungeSaber.UI.BSML.Leaderboard
 {
     [ViewDefinition("LoungeSaber.UI.BSML.Leaderboard.LeaderboardPanelView.bsml")]
-    public class LeaderboardPanelViewController : BSMLAutomaticViewController
+    public class LeaderboardPanelViewController : BSMLAutomaticViewController, IInitializable, IDisposable
     {
         [Inject] private readonly LoungeSaberApi _loungeSaberApi = null;
         [Inject] private readonly SiraLog _siraLog = null;
+        
+        [Inject] private readonly LoungeSaberLeaderboardViewController _loungeSaberLeaderboardViewController = null;
         
         private bool _isLoading = false;
         
@@ -28,11 +32,13 @@ namespace LoungeSaber.UI.BSML.Leaderboard
         [UIValue("showRankingScreen")] private bool _showRankingScreen => !IsLoading;
         
         
-        [UIValue("globalRankingText")] private string _globalRankingText = "placeholder";
-        [UIValue("divisionText")] private string _divisionText = "placeholder";
+        [UIValue("globalRankingText")] private string _globalRankingText { get; set; } = "placeholder";
+        [UIValue("divisionText")] private string _divisionText { get; set; } = "placeholder";
 
-        [UIValue("bgColor")] private string _bgColor = "#FF0000";
+        [UIValue("bgColor")] private string _bgColor { get; set; } = "#808080";
         
+        private FloatingScreen _floatingScreen;
+
         public async Task UpdateRankingInfo()
         {
             IsLoading = true;
@@ -41,12 +47,57 @@ namespace LoungeSaber.UI.BSML.Leaderboard
             
             var userData = await _loungeSaberApi.GetUserInfo(userId);
 
-            _globalRankingText = $"Global Ranking: #{(userData == null ? "0" : userData.Rank)}";
+            _globalRankingText = $"<b>Global Ranking:</b> #{(userData == null ? "0" : userData.Rank)}";
             _divisionText = "placeholder"; //TODO: waiting on garrick
             
             IsLoading = false;
             
             NotifyPropertyChanged(null);
+        }
+
+        private async void OnLeaderboardActivated(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            try
+            {
+                if (firstActivation)
+                {
+                    CreateFloatingLeaderboardPanel();
+                    _floatingScreen.gameObject.SetActive(true);
+                    await UpdateRankingInfo();
+                    return;
+                }
+            
+                _floatingScreen.gameObject.SetActive(true);
+            }
+            catch (Exception e)
+            {
+                _siraLog.Error(e);
+            }
+        }
+
+        private void OnLeaderboardDeactivated(bool removedFromHierarchy, bool screenSystemDisabling) => _floatingScreen.gameObject.SetActive(false);
+
+        private void CreateFloatingLeaderboardPanel()
+        {
+            _floatingScreen = FloatingScreen.CreateFloatingScreen(new Vector2(100f, 25f), false, Vector2.zero, Quaternion.identity);
+            _floatingScreen.SetRootViewController(this, AnimationType.In);
+            _floatingScreen.name = "LoungeSaberFloatingScreen";
+            
+            _floatingScreen.transform.SetParent(, false);
+            _floatingScreen.transform.localPosition = new Vector3(3f, 50f);
+            _floatingScreen.transform.localScale = Vector3.one;
+        }
+
+        public void Initialize()
+        {
+            _loungeSaberLeaderboardViewController.didDeactivateEvent += OnLeaderboardDeactivated;
+            _loungeSaberLeaderboardViewController.didActivateEvent += OnLeaderboardActivated;
+        }
+
+        public void Dispose()
+        {
+            _loungeSaberLeaderboardViewController.didActivateEvent -= OnLeaderboardActivated;
+            _loungeSaberLeaderboardViewController.didDeactivateEvent -= OnLeaderboardDeactivated;
         }
     }
 }
--- LoungeSaber/UI/BSML/Leaderboard/LoungeSaberLeaderboardViewController.cs
+++ LoungeSaber/UI/BSML/Leaderboard/LoungeSaberLeaderboardViewController.cs
@@ -14,39 +14,6 @@ namespace LoungeSaber.UI.BSML.Leaderboard
     [ViewDefinition("LoungeSaber.UI.BSML.Leaderboard.LoungeSaberLeaderboardView.bsml")]
     public class LoungeSaberLeaderboardViewController : BSMLAutomaticViewController
     {
-        [Inject] private readonly LeaderboardPanelViewController _leaderboardPanelViewController = null;
-        [Inject] private readonly SiraLog _siraLog = null;
         
-        private FloatingScreen _floatingScreen;
-        
-        protected override async void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
-        {
-            base.DidActivate(firstActivation, addedToHierarchy, screenSystemEnabling);
-            try
-            {
-                if (firstActivation)
-                {
-                    CreateFloatingLeaderboardPanel();
-                    _floatingScreen.gameObject.SetActive(true);
-                    await _leaderboardPanelViewController.UpdateRankingInfo();
-                    return;
-                }
-            
-                _floatingScreen.gameObject.SetActive(true);
-            }
-            catch (Exception e)
-            {
-                _siraLog.Error(e);
-            }
-        }
-        
-        protected override void DidDeactivate(bool removedFromHierarchy, bool screenSystemEnabling) => _floatingScreen.gameObject.SetActive(false);
-        
-        private void CreateFloatingLeaderboardPanel()
-        {
-            _floatingScreen = FloatingScreen.CreateFloatingScreen(new Vector2(100f, 25f), true, Vector2.zero, Quaternion.identity);
-            _floatingScreen.SetRootViewController(_leaderboardPanelViewController, AnimationType.In);
-            _floatingScreen.name = "LoungeSaberFloatingScreen";
-        }
     }
 }
