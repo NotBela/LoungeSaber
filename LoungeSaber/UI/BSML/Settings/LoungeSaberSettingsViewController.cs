463260a2bcab0406b2b99cf425fd7cde19075aeb
Bela <27pvarga@gmail.com>
2025-09-02 02:43:57 -0400

    added settings view controller

---
+++ LoungeSaber/UI/BSML/Settings/LoungeSaberSettingsViewController.cs
@@ -0,0 +1,22 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Settings;
+using BeatSaberMarkupLanguage.ViewControllers;
+using Zenject;
+
+namespace LoungeSaber.UI.BSML.Settings;
+
+[ViewDefinition("LoungeSaber.UI.BSML.Settings.LoungeSaberSettingsView.bsml")]
+public class LoungeSaberSettingsViewController : BSMLAutomaticViewController, IInitializable, IDisposable
+{
+    [Inject] private readonly BSMLSettings _bsmlSettings = null;
+    
+    public void Initialize()
+    {
+        _bsmlSettings.AddSettingsMenu("LoungeSaber", "LoungeSaber.UI.BSML.Settings.LoungeSaberSettingsView.bsml", this);
+    }
+
+    public void Dispose()
+    {
+        _bsmlSettings.RemoveSettingsMenu("LoungeSaber");
+    }
+}
