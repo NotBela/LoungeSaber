bc3c6d471183fe9f51bfa90cb7c2e5ebb29c8220
Bela <27pvarga@gmail.com>
2025-09-05 23:45:21 -0400

    finally fixed it for real this time i think

--- LoungeSaber/Game/MatchManager.cs
+++ LoungeSaber/Game/MatchManager.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using LoungeSaber.Models.Map;
 using LoungeSaber.Server;
-using LoungeSaber.UI.ViewManagers;
+using LoungeSaber.UI.BSML.PauseMenu;
 using SiraUtil.Logging;
 using UnityEngine.SceneManagement;
 using Zenject;
@@ -13,16 +14,20 @@ namespace LoungeSaber.Game
 {
     public class MatchManager : IInitializable, IDisposable
     {
-        [Inject] private readonly MenuTransitionsHelper _menuTransitionsHelper = null;
-        [Inject] private readonly PlayerDataModel _playerDataModel = null;
-        [Inject] private readonly SiraLog _siraLog = null;
+        [Inject] private readonly MenuTransitionsHelper _menuTransitionsHelper = null!;
+        [Inject] private readonly PlayerDataModel _playerDataModel = null!;
+        [Inject] private readonly SiraLog _siraLog = null!;
         
-        [Inject] private readonly ServerListener _serverListener = null;
+        [Inject] private readonly ServerListener _serverListener = null!;
         
         public bool InMatch { get; private set; } = false;
 
-        public event Action<LevelCompletionResults, StandardLevelScenesTransitionSetupDataSO> OnLevelCompleted; 
+        [CanBeNull] public Models.UserInfo.UserInfo Opponent { get; private set; }
 
+        public event Action<LevelCompletionResults, StandardLevelScenesTransitionSetupDataSO> OnLevelCompleted;
+
+        public void SetOpponent(Models.UserInfo.UserInfo opponent) => Opponent = opponent;
+        
         public void StartMatch(VotingMap level, DateTime unpauseTime, bool proMode)
         {
             if (InMatch) 
@@ -31,45 +36,8 @@ namespace LoungeSaber.Game
             InMatch = true;
             
             var beatmapLevel = level.GetBeatmapLevel() ?? throw new Exception("Could not get beatmap level!");
-            BeatmapDifficulty difficulty;
-            
-            switch (level.Difficulty)
-            {
-                case VotingMap.DifficultyType.Easy:
-                    difficulty = BeatmapDifficulty.Easy;
-                    break;
-                case VotingMap.DifficultyType.Normal:
-                    difficulty = BeatmapDifficulty.Normal;
-                    break;
-                case VotingMap.DifficultyType.Hard:
-                    difficulty = BeatmapDifficulty.Hard;
-                    break;
-                case VotingMap.DifficultyType.Expert:
-                    difficulty = BeatmapDifficulty.Expert;
-                    break;
-                default:
-                    difficulty = BeatmapDifficulty.ExpertPlus;
-                    break;
-            }
+            var difficulty = level.GetBaseGameDifficultyType();
 
-            /*_menuTransitionsHelper.StartStandardLevel(
-                "Solo",
-                beatmapLevel.GetBeatmapKeys().First(i => i.beatmapCharacteristic.serializedName == "Standard" && i.difficulty == difficulty),
-                beatmapLevel,
-                _playerDataModel.playerData.overrideEnvironmentSettings,
-                _playerDataModel.playerData.colorSchemesSettings.overrideDefaultColors ? _playerDataModel.playerData.colorSchemesSettings.GetSelectedColorScheme() : null,
-                null,
-                new GameplayModifiers(GameplayModifiers.EnergyType.Bar, true, false, false, GameplayModifiers.EnabledObstacleType.All, false, false, false, false, GameplayModifiers.SongSpeed.Normal, false, false, proMode, false, false),
-                _playerDataModel.playerData.playerSpecificSettings,
-                null,
-                EnvironmentsListModel.CreateFromAddressables(),
-                "Menu",
-                true,
-                diContainer => AfterSceneSwitchToGameplayCallback(diContainer, unpauseTime),
-                AfterSceneSwitchCallback,
-                null
-                );*/
-            
             _menuTransitionsHelper.StartStandardLevel(
                 "Solo", 
                 beatmapLevel.GetBeatmapKeys().First(i => i.beatmapCharacteristic.serializedName == "Standard" && i.difficulty == difficulty),
@@ -86,23 +54,25 @@ namespace LoungeSaber.Game
                 false,
                 true,
                 null,
+                // TODO: fix restart button being visible
                 diContainer => AfterSceneSwitchToGameplayCallback(diContainer, unpauseTime),
-                AfterSceneSwitchFromGameplayCallback,
+                AfterSceneSwitchCallback,
                 null
-                );
+            );
         }
 
-        private void AfterSceneSwitchFromGameplayCallback(StandardLevelScenesTransitionSetupDataSO levelScenesTransitionSetupDataSo, LevelCompletionResults completionResults)
+        private void AfterSceneSwitchCallback(StandardLevelScenesTransitionSetupDataSO standardLevelScenesTransitionSetupDataSo, LevelCompletionResults levelCompletionResults)
         {
             InMatch = false;
-            
-            OnLevelCompleted?.Invoke(completionResults, levelScenesTransitionSetupDataSo);
+            OnLevelCompleted?.Invoke(levelCompletionResults, standardLevelScenesTransitionSetupDataSo);
         }
 
         private async void AfterSceneSwitchToGameplayCallback(DiContainer diContainer, DateTime unpauseTime)
         {
             try
             {
+                diContainer.Resolve<PauseMenuViewController>().SetMatchStartingTime(unpauseTime);
+                
                 var startingMenuController = diContainer.TryResolve<MatchStartUnpauseController>() ?? throw new Exception("Could not resolve StartingPauseMenuController");
 
                 await startingMenuController.UnpauseLevelAtTime(unpauseTime);
